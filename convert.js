2f753611bd269635f68994917d08e86e0d025df1
Niklauzi <ftpfavour@gmail.com>
2023-10-26 03:51:57 +0100

    second commit

--- convert.js
+++ convert.js
@@ -1,92 +1,37 @@
-// // Get HTML ELEMENTS
-// const inputElement = document.getElementById('inCur');
-// const outputElement = document.getElementById('outCur');
-// const convertButton = document.querySelector('button');
-// const resultElement = document.getElementById('result');
-
-// const inputValue = () => {
-//   const inputValue = inputElement.value;
-//   console.log(inputValue);
-
-//   // Fetch the JSON file as text
-//   fetch('api.json')
-//     .then((response) => response.text())
-//     .then((jsonText) => {
-//       // Parse the JSON text into a JavaScript object
-//       const jsonData = JSON.parse(jsonText);
-
-//       // Find the object in the JSON data based on the user's input
-//       const foundToken = jsonData.find(
-//         (token) =>
-//           token.symbol.toLowerCase() === inputValue ||
-//           token.name.toLowerCase() === inputValue
-//       );
-//       //  TO console
-//       foundToken
-//         ? console.log(
-//             `${foundToken.symbol}: ${foundToken.price} is No ${
-//               jsonData.indexOf(foundToken) + 1
-//             } king of DeFi`
-//           )
-//         : console.log(
-//             `Token with symbol or name '${inputValue}' not found in JSON data.`
-//           );
-
-//       // TO DOM
-//       if (foundToken) {
-//         const resultText = `${foundToken.symbol}: $${foundToken.price}`;
-
-//         // Set the result in the resultElement
-//         resultElement.textContent = resultText;
-//       } else {
-//         resultElement.textContent = `Token with symbol or name '${inputValue}' not found in database.`;
-//       }
-//     })
-//     .catch((error) => {
-//       console.error('Error fetching JSON:', error);
-//     });
-// };
-
-// // inputElement.addEventListener('input', () => {
-// //   inputValue();
-// // });
-
-// convertButton.addEventListener('click', () => {
-//   inputValue();
-// });
-
-// inputElement.addEventListener('keyup', (event) => {
-//   if (event.key === 'Enter') {
-//     inputValue();
-//   }
-// });
-
 console.log('triton');
 
-const inputElement = document.getElementById('tokenInput');
-const convertButton = document.getElementById('convertButton');
+// const convertButton = document.getElementById('convertButton');
 const resultElement = document.getElementById('result');
-const tokenDropdown = document.getElementById('tokenDropdown');
-const searchInput = document.getElementById('search');
+const tokenDropdown1 = document.getElementById('tokenDropdown1');
+const tokenDropdown2 = document.getElementById('tokenDropdown2');
 
 // Function to fetch token data from api.json
 const fetchTokenData = () => {
-  const inputValue = inputElement.value.toLowerCase();
+  const selectedToken1 = tokenDropdown1.value;
+  const selectedToken2 = tokenDropdown2.value;
 
   fetch('api.json')
     .then((response) => response.json())
     .then((jsonData) => {
-      const foundToken = jsonData.find(
+      const foundToken1 = jsonData.find(
         (token) =>
-          token.symbol.toLowerCase() === inputValue ||
-          token.name.toLowerCase() === inputValue
+          token.symbol.toLowerCase() === selectedToken1.toLowerCase() ||
+          token.name.toLowerCase() === selectedToken1.toLowerCase()
+      );
+      const foundToken2 = jsonData.find(
+        (token) =>
+          token.symbol.toLowerCase() === selectedToken2.toLowerCase() ||
+          token.name.toLowerCase() === selectedToken2.toLowerCase()
       );
 
-      if (foundToken) {
-        const resultText = `${foundToken.symbol}: $${foundToken.price}`;
+      if (foundToken1 && foundToken2) {
+        const conversionRate = foundToken1.price / foundToken2.price;
+        const resultText = `1 $${foundToken1.symbol} = ${conversionRate.toFixed(
+          8
+        )} $${foundToken2.symbol}`;
         resultElement.textContent = resultText;
       } else {
-        resultElement.textContent = `Token with symbol or name '${inputValue}' not found in database.`;
+        resultElement.textContent = `Token not found in the database.`;
       }
     })
     .catch((error) => {
@@ -94,45 +39,34 @@ const fetchTokenData = () => {
     });
 };
 
-// Populate the dropdown with tokens from api.json
+// Populate the dropdowns with tokens from api.json
 fetch('api.json')
   .then((response) => response.json())
   .then((jsonData) => {
     jsonData.forEach((token) => {
-      const option = document.createElement('option');
-      option.value = token.symbol;
-      option.textContent = token.name;
-      tokenDropdown.appendChild(option);
+      const option1 = document.createElement('option');
+      option1.value = token.symbol;
+      option1.textContent = token.name;
+      tokenDropdown1.appendChild(option1);
+
+      const option2 = document.createElement('option');
+      option2.value = token.symbol;
+      option2.textContent = token.name;
+      tokenDropdown2.appendChild(option2);
     });
   })
   .catch((error) => {
-    console.error('Error fetching JSON for dropdown:', error);
+    console.error('Error fetching JSON for dropdowns:', error);
   });
-
-searchInput.addEventListener('input', function () {
-  const searchTerm = searchInput.value.toLowerCase();
-
-  for (let option of tokenDropdown.options) {
-    if (option.value.toLowerCase().includes(searchTerm)) {
-      option.style.display = 'block';
-    } else {
-      option.style.display = 'none';
-    }
-  }
-});
-
 // Event listeners
-convertButton.addEventListener('click', () => {
+// convertButton.addEventListener('click', () => {
+//   fetchTokenData();
+// });
+
+tokenDropdown1.addEventListener('change', () => {
   fetchTokenData();
 });
 
-inputElement.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    fetchTokenData();
-  }
-});
-
-tokenDropdown.addEventListener('change', () => {
-  inputElement.value = tokenDropdown.value;
+tokenDropdown2.addEventListener('change', () => {
   fetchTokenData();
 });
